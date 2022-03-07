2bf9f420d42334ee2db35eaae9d2aa55880da147
Brian Garland <briangarlandk@gmail.com>
2022-03-07 17:21:32 -0600

    Mutations added to client side

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,64 @@
+import { gql } from '@apollo/client';
+
+export const USER_LOGIN = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $emai, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($email: String!, $password: String!, $username: String!) {
+        addUser(email: $email, password: $password, username: $username) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount 
+            savedBooks {
+                bookId
+                authors
+                title
+                description
+                image
+                link
+            }
+        }
+    }
+`
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: savedBook!) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            bookCount 
+            savedBooks {
+                bookId
+                authors
+                title
+                description
+                image
+                link
+            }
+        }
+    }
+`
